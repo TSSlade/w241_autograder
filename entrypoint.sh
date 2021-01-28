0909ced36dbf8423fa32b3daa2aabdd99da94761
TSSlade <tslade@berkeley.edu>
2021-01-28 09:22:52 -0800

    Initial commit of GitHub Action Docker MWE

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+# Adapted from GitHub Actions MWE for Docker containers
+# https://docs.github.com/en/actions/creating-actions/creating-a-docker-container-action
+
+#!/bin/sh -l
+
+echo "Hello $1"
+time=$(date)
+echo "::set-output name=time::$time"
