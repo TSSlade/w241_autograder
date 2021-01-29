39ac4bb35aeb6f4b6d69766a82bcb0c8bd77f763
TSSlade <tslade@berkeley.edu>
2021-01-28 19:52:37 -0800

    Add chmod +x to Dockerfile, move up shebang in entrypoint

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -1,7 +1,7 @@
+#!/bin/sh -l
+
 # Adapted from GitHub Actions MWE for Docker containers
 # https://docs.github.com/en/actions/creating-actions/creating-a-docker-container-action
-
-#!/bin/sh -l
 
 echo "Hello $1"
 time=$(date)
