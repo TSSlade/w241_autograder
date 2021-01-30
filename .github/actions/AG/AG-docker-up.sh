e28497acb47f85c01f0562cea3b247f8f00afc06
TSSlade <tslade@berkeley.edu>
2021-01-30 15:17:25 -0800

    Slow down again

--- .github/actions/AG/AG-docker-up.sh
+++ .github/actions/AG/AG-docker-up.sh
@@ -1,26 +1,26 @@
 #!/bin/sh -l
 
 echo "Docker container is up!"
-echo "We're here:"
-pwd
+echo "++++ We're here: ${pwd}"
 echo "This is what we're working with:"
 # tree -L 2 .
 ls -lah
-echo "Does this contain what we hope it does?"
-# tree -L 2 .
-ls -lah ./TSSlade
-echo "And here's the stuff in the parent volume:"
-# tree -L "${HOME}"
-ls -lah ../
-echo "And here's the stuff in the grandparent volume:"
-# tree -L "${HOME}"
+cd ../
+echo "Up 1: ${pwd}"
+ls -lah .
+cd ../
+echo "Up 2: ${pwd}"
+ls -lah .
+cd ../
+echo "Up 3: ${pwd}"
+ls -lah .
 ls -lah ../../
-echo "And here's the stuff in the Auntie Workspace volume:"
-# tree -L "${HOME}"
-ls -lah ../../workspace/
-echo "And here's the stuff in the Auntie Home volume:"
-# tree -L "${HOME}"
-ls -lah ../../home/
-echo "And here's the stuff in the Cousin mnt volume:"
-# tree -L "${HOME}"
-ls -lah ../../home/mnt/
+# echo "And here's the stuff in the Auntie Workspace volume:"
+# # tree -L "${HOME}"
+# ls -lah ../../workspace/
+# echo "And here's the stuff in the Auntie Home volume:"
+# # tree -L "${HOME}"
+# ls -lah ../../home/
+# echo "And here's the stuff in the Cousin mnt volume:"
+# # tree -L "${HOME}"
+# ls -lah ../../home/mnt/
