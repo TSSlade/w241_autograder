37c90511aa6207086f68255756377ff2712f066a
TSSlade <tslade@berkeley.edu>
2021-01-30 16:34:55 -0800

    Using artifacts now

--- .github/actions/AG/AG-docker-up.sh
+++ .github/actions/AG/AG-docker-up.sh
@@ -1,19 +1,19 @@
 #!/bin/sh -l
 
 echo "Docker container is up!"
-echo "++++ We're here:"
-pwd
-ls -lah
-cd ../
-echo "Up 1:"
-pwd
-tree -L 2
-echo "In github/home:"
-ls -lah ./home
-echo "In ${HOME}:"
-ls -lah $HOME
-echo "In /home/runner/"
-tree -L 1 /home/runner
+# echo "++++ We're here:"
+# pwd
+# ls -lah
+# cd ../
+# echo "Up 1:"
+# pwd
+# tree -L 2
+# echo "In github/home:"
+# ls -lah ./home
+# echo "In ${HOME}:"
+# ls -lah $HOME
+# echo "In /home/runner/"
+# tree -L 1 /home/runner
 
 # echo "And here's the stuff in the Auntie Workspace volume:"
 # # tree -L "${HOME}"
